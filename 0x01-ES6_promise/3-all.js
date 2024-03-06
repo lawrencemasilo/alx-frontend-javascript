978896c7b4233488fa6b61a60c1bc8e9ff74cab6
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-06 12:19:34 +0200

    importing files

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from '../utils';
+
+export default async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body.firstName} ${user.body.firstName} ${user.body.lastName}`);
+  } catch (error) {
+    console.error('Signup system offline');
+  }
+}
