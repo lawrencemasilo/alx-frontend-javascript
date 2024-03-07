25b1acd4ebd2d640194b73d6523507f8d8529d5c
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-07 14:29:29 +0200

    Car clone

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-underscore-dangle
+const _brand = Symbol('brand');
+// eslint-disable-next-line no-underscore-dangle
+const _motor = Symbol('motor');
+// eslint-disable-next-line no-underscore-dangle
+const _color = Symbol('color');
+
+export default class Car {
+  constructor(brand, motor, color) {
+    // eslint-disable-next-line no-underscore-dangle
+    this[_brand] = brand;
+    // eslint-disable-next-line no-underscore-dangle
+    this[_motor] = motor;
+    // eslint-disable-next-line no-underscore-dangle
+    this[_color] = color;
+  }
+
+  cloneCar() {
+    // eslint-disable-next-line no-underscore-dangle
+    return new Car(this[_brand], this[_motor], this[_color]);
+  }
+}
