2ea7e68616ab33843410eff787e3b488966bcf29
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-07 12:38:58 +0200

    Implemented class Currency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code === 'string') {
+      // eslint-disable-next-line no-underscore-dangle
+      this._code = code;
+    }
+    if (typeof name === 'string') {
+      // eslint-disable-next-line no-underscore-dangle
+      this._name = name;
+    }
+  }
+
+  get code() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      // eslint-disable-next-line no-underscore-dangle
+      this._code = newCode;
+    }
+  }
+
+  get name() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      // eslint-disable-next-line no-underscore-dangle
+      this._name = newName;
+    }
+  }
+}
