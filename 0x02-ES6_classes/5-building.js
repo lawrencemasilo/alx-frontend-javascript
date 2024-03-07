5ab02e198bf6d151098b39a1e7c11a07ad80fb85
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-07 13:31:03 +0200

    Implemented a class named Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft === 'number') {
+      // eslint-disable-next-line no-underscore-dangle
+      this._sqft = sqft;
+    }
+  }
+
+  get sqft() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    // eslint-disable-next-line no-underscore-dangle
+    if (this._sqft) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+}
