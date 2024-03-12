9656feb009ca4082065ea8ff7e0fd240d30ecfea
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-12 16:07:15 +0200

    returns each id of an object in an array

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
 export default function getListStudentIds(arr) {
   if (Array.isArray(arr)) {
-    const newArr = [];
-
-    arr.map((obj) => newArr.append(obj.id));
-
-    return newArr;
+    return arr.map((obj) => obj.id);
   }
 
   return [];
