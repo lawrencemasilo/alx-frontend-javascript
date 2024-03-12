8d57115a5f5ab8c0ef67000a788254cc9df11ce6
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-12 16:51:39 +0200

    returns the sum of all the student ids

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(arr) {
   if (Array.isArray(arr)) {
-    return arr.reduce((sum, currentValue) => sum.id + currentValue.id);
+    return arr.reduce((sum, currentValue) => sum + currentValue.id, 0);
   }
 
   return [];
