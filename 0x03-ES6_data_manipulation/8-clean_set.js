712ec257d705167488124b04c7bd343a863deed3
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-03-12 18:55:20 +0200

    returns a string of all the set values that start with a specific string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,8 @@ export default function cleanSet(set, searchString) {
   const result = [];
   for (const element of set) {
     if (element.startsWith(searchString)) {
-      result.push(set.substring(searchString.length));
+      result.push(element.substring(searchString.length));
     }
   }
-  result.join('-');
-  return result[0].toString();
+  return result.join('-').length > 0 ? result.join('-') : '';
 }
