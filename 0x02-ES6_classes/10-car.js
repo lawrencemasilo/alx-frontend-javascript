b980309a80cc57cc1622d3b47f1b7b76ceb9e26e
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-05-21 13:27:58 +0200

    update

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -15,6 +15,21 @@ export default class Car {
     this[_color] = color;
   }
 
+  get brand() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this[_brand];
+  }
+
+  get motor() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this[_motor];
+  }
+
+  get color() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this[_color];
+  }
+
   cloneCar() {
     // eslint-disable-next-line no-underscore-dangle
     return new this.constructor(this[_brand], this[_motor], this[_color]);
